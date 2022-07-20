05825dacedda5e7fb2353f2800717e4d8a804d5d
ruslanzverkov <createmed@mail.ru>
2022-07-20 19:58:58 +0300

    DZ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        //        Первое задание
+        int i=1;
+        while(i<=10){
+            System.out.print(i);
+            i++;
+        }
+        System.out.println();
+
+
+        for( i=10;i>0;i=i-1){
+            System.out.print(i);
+        }
+        System.out.println();
     }
 }
